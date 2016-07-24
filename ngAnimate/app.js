8c5373403e27e53c51ce823d626bd0fdca52d1d9
Dessagn <temesgen.dessalegn@gmail.com>
2016-07-24 22:20:15 +0300

    ngAnimate examples commit

---
+++ ngAnimate/app.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// app.js
+
+// define our application and pull in ngRoute and ngAnimate
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+
+// ROUTING ===============================================
+// set our routing for this application
+// each route will pull in a different controller
+animateApp.config(function ($routeProvider) {
+
+    $routeProvider
+
+            // home page
+            .when('/', {
+                templateUrl: 'page-home.html',
+                controller: 'mainController'
+            })
+
+            // about page
+            .when('/about', {
+                templateUrl: 'page-about.html',
+                controller: 'aboutController'
+            })
+
+            // contact page
+            .when('/contact', {
+                templateUrl: 'page-contact.html',
+                controller: 'contactController'
+            });
+
+});
+
+
+// CONTROLLERS ============================================
+// home page controller
+animateApp.controller('mainController', function ($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+// about page controller
+animateApp.controller('aboutController', function ($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+// contact page controller
+animateApp.controller('contactController', function ($scope) {
+    $scope.pageClass = 'page-contact';
+});
